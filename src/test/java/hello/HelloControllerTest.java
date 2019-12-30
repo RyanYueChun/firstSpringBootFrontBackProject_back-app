d7e4285d833e79501ade37b0ca42a1749ecb040a
Ryan Yue Chun <ryan.yue-chun@outlook.com>
2019-12-30 15:10:47 +0100

    Added a RESTful 'GreetingCOntroller' WebService to send a response according to the parameter containing request made by the front.

--- src/test/java/hello/HelloControllerTest.java
+++ src/test/java/hello/HelloControllerTest.java
@@ -4,6 +4,7 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.google.gson.Gson;
 import org.json.JSONObject;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -29,6 +30,21 @@ public class HelloControllerTest {
         mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().string(equalTo("{\"contents\":\"Greetings from Spring Boot!\"}")));
+    }
+
+    @Test
+    public void getGreeting() throws Exception {
+        Greeting expectedGreeting = new Greeting(1, "Test_user");
+        Gson expectedJson = new Gson();
+
+
+        mvc.perform(MockMvcRequestBuilders.get("/greeting").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("{\"id\":1,\"content\":\"Hello, World!\"}")));
+
+        mvc.perform(MockMvcRequestBuilders.get("/greeting?name=Test_user").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("{\"id\":2,\"content\":\"Hello, Test_user!\"}")));
     }
 }
 
